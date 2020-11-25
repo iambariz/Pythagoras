357ecae46fe8d011e51c82a7d6440fb3c03bf133
iambariz <61651210+iambariz@users.noreply.github.com>
2020-11-25 15:16:00 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pythagorean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give hypotenuse side");
+            double cSide = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please give known side side");
+            double aSide = Convert.ToDouble(Console.ReadLine());
+            double bSide = (cSide * 2) - (aSide * 2);
+            Console.WriteLine(bSide);
+
+        }
+    }
+}
